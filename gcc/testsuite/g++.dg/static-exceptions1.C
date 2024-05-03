a248fcfd282981094d78552ac5ce895e649bd74b
feedable <141534996+feedab1e@users.noreply.github.com>
2024-05-03 23:21:09 +0300

    Add test

---
+++ gcc/testsuite/g++.dg/static-exceptions1.C
@@ -0,0 +1,72 @@
+/* testing the correct usage static exceptions.  */
+/* { dg-do compile { target c++26 } } */
+/* { dg-options "-fstatic-exceptions" } */
+
+template<class, class>
+struct same;
+
+template<class T>
+struct same<T, T> {};
+
+int test_empty_spec() throw() {
+  return 1;
+}
+
+void helper_throws_int() throw(int) {}
+void helper_throws_float() throw(float) {}
+
+int test_empty_spec_throws() throw() {
+  helper_throws_int();
+  return 1;
+} /* { dg-error "Invalid eh spec !!!" } */
+
+
+int test_valid_throws_int() throw(int) {
+  throw 1;
+}
+
+int test_mismatched_type() throw(float) {
+  test_valid_throws_int();
+  return 1;
+} /* { dg-error "Invalid eh spec !!!" } */
+
+int test_deduce() throw(auto) {
+  throw 1.;
+}
+
+int test_check_deduce() throw(double) {
+  test_deduce();
+  return 1;
+}
+int test_check_deduce_not_noexcept() throw() {
+  test_deduce();
+  return 1;
+} /* { dg-error "Invalid eh spec !!!" } */
+
+void check_try_valid() noexcept {try {
+    helper_throws_float();
+}
+  catch(float) {}
+}
+
+void check_try_invalid() noexcept {try {
+    helper_throws_float();
+}
+  catch(int) {}
+}/* { dg-error "Invalid eh spec !!!" } */
+void check_try_all() noexcept {try {
+    helper_throws_float();
+}
+  catch(...) {}
+}
+
+void check_catch_auto() noexcept {
+  try {
+    helper_throws_float();
+    helper_throws_int();
+  }
+  catch (auto x) {
+	same<decltype(x), int>{};/* { dg-error "invalid use of incomplete type" } */
+	same<decltype(x), float>{};/* { dg-error "invalid use of incomplete type" } */
+  }
+}
